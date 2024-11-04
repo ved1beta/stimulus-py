c69e15a876c283f7d7b8a1c2975c2ea5108ebe3e
itrujnara <itrujnara@wp.pl>
2024-11-04 15:44:48 +0100

    Rewrite splitters docs to the Google format

--- src/stimulus/data/splitters/splitters.py
+++ src/stimulus/data/splitters/splitters.py
@@ -10,21 +10,45 @@ import numpy as np
 class AbstractSplitter(ABC):
     """
     Abstract class for splitters.
+
+    A splitter splits the data into train, validation, and test sets.
+
+    Methods:
+        get_split_indexes: calculates split indices for the data
+        distance: calculates the distance between two elements of the data
     """
 
     @abstractmethod
     def get_split_indexes(self, data: pl.DataFrame, seed: float = None ) -> list:
         """
-        Splits the data. Always return indices mapping to the original list. 
+        Splits the data. Always return indices mapping to the original list.
+
+        This is an abstract method that should be implemented by the child class.
+
+        Args:
+            data (pl.DataFrame): the data to be split
+            seed (float): the seed for reproducibility
+
+        Returns:
+            split_indices (list): the indices for train, validation, and test sets
         """
         raise NotImplementedError
     
-    # @abstractmethod
-    # def distance(self, data_one: Any, data_two: Any) -> float:
-    #     """
-    #     Calculates the distance between two elements of the data.
-    #     """+
-    #     raise NotImplementedError
+    @abstractmethod
+    def distance(self, data_one: Any, data_two: Any) -> float:
+        """
+        Calculates the distance between two elements of the data.
+
+        This is an abstract method that should be implemented by the child class.
+
+        Args:
+            data_one (Any): the first data point
+            data_two (Any): the second data point
+
+        Returns:
+            distance (float): the distance between the two data points
+        """
+        raise NotImplementedError
     
 
 class RandomSplitter(AbstractSplitter):
@@ -35,25 +59,21 @@ class RandomSplitter(AbstractSplitter):
     def __init__(self) -> None:
         super().__init__()
 
-    def get_split_indexes(self, data: pl.DataFrame, split: list = [0.7, 0.2, 0.1], seed: float = None) -> list | list | list:
+    def get_split_indexes(self, data: pl.DataFrame, split: list = [0.7, 0.2, 0.1], seed: float = None) -> tuple[list, list, list]:
         """
         Splits the data indices into train, validation, and test sets. 
+        
         One can use these lists of indices to parse the data afterwards.
 
-        args:
-            data: polars dataframe
-                The data loaded with polars.
-            split: list
-                The proportions for [train, validation, test] splits.
-            seed: float
-                The seed for reproducibility.
-        returns:
-            train: list
-                The indices for the training set.
-            validation: list
-                The indices for the validation set.
-            test: list
-                The indices for the test set.
+        Args:
+            data (pl.DataFrame): The data loaded with polars.
+            split (list): The proportions for [train, validation, test] splits.
+            seed (float): The seed for reproducibility.
+
+        Returns:
+            train (list): The indices for the training set.
+            validation (list): The indices for the validation set.
+            test (list): he indices for the test set.
         """
         if len(split) != 3:
             raise ValueError("The split argument should be a list with length 3 that contains the proportions for [train, validation, test] splits.")
