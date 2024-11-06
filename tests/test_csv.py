0b44b6a7f23fb8dfaa7d4b11214c21296893c094
Suzanne Jin <suzanne.jin.yao@gmail.com>
2024-11-06 18:56:53 +0100

    add first tests in test_csv.py

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,3 +1,4 @@
+import pytest
 import json
 import os
 from abc import ABC
@@ -5,102 +6,96 @@ from abc import ABC
 import numpy as np
 import numpy.testing as npt
 import polars as pl
-import pytest
 
 from src.stimulus.data.csv import CsvLoader, CsvProcessing
 from src.stimulus.data.experiments import DnaToFloatExperiment, ProtDnaToFloatExperiment
 
 
+# set seed for reproducibility
+SEED = 123
+# np.random.seed(SEED)
+# pl.set_random_seed(SEED)
+
+# ----- set up objects ----- #
+
+class csv_data():
+    """ This is a class to initialize the corresponding classes for a given csv data. """
+    def __init__(self, filename, experiment):
+        self.experiment = experiment()
+        self.csv_path = os.path.abspath(filename)
+        self.csv_processing = CsvProcessing(self.experiment, self.csv_path)
+        self.data_length = None
+        self.values = None
+
 @pytest.fixture
-def dna_float_setup():
-    np.random.seed(123)
-    pl.set_random_seed(123)
-    experiment = DnaToFloatExperiment()
-    csv_path = os.path.abspath("tests/test_data/dna_experiment/test.csv")
-    csv_processing = CsvProcessing(experiment, csv_path)
-    
-    csv_shuffle_long_path = os.path.abspath("tests/test_data/dna_experiment/test_shuffling_long.csv")
-    csv_shuffle_long = CsvProcessing(experiment, csv_shuffle_long_path)
-    
-    csv_shuffle_long_shuffled_path = os.path.abspath("tests/test_data/dna_experiment/test_shuffling_long_shuffled.csv")
-    csv_shuffle_long_shuffled = CsvProcessing(experiment, csv_shuffle_long_shuffled_path)
-    
-    with open("tests/test_data/dna_experiment/test_config.json") as f:
-        configs = json.load(f)
-    
-    return {
-        "csv_processing": csv_processing,
-        "configs": configs,
-        "data_length": 2,
-        "expected_splits": [1, 0],
-        "csv_shuffle_long": csv_shuffle_long,
-        "csv_shuffle_long_shuffled": csv_shuffle_long_shuffled
+def dna_test_data():
+    """ This is the csv_data class storing the basic dna test csv """
+    data = csv_data("tests/test_data/dna_experiment/test.csv", DnaToFloatExperiment)
+    data.data_length = 2
+    data.values = {
+        "pet:meta:str" : ["cat", "dog", "cat", "dog"],
+        "hola:label:float" : [12.676405, 12.540016, 12.676405, 12.540016],
+        "hello:input:dna" : ["ACTGACTGATCGATNN", "ACTGACTGATCGATNN", "NNATCGATCAGTCAGT", "NNATCGATCAGTCAGT"]
     }
+    return data
 
-# @pytest.fixture
-# def prot_dna_float_setup():
-#     experiment = ProtDnaToFloatExperiment()
-#     csv_path = os.path.abspath("tests/test_data/prot_dna_experiment/test.csv")
-#     csv_processing = CsvProcessing(experiment, csv_path)
+@pytest.fixture
+def dna_test_data_long():
+    """ This is the csv_data class storing the long dna test csv """
+    data = csv_data("tests/test_data/dna_experiment/test_shuffling_long.csv", DnaToFloatExperiment)
+    data.data_length = 1000
+    return data
+
+@pytest.fixture
+def dna_test_data_long_shuffled():
+    """ This is the csv_data class storing the shuffled long dna test csv """
+    data = csv_data("tests/test_data/dna_experiment/test_shuffling_long_shuffled.csv", ProtDnaToFloatExperiment)
+    data.data_length = 1000
+    return data
+
+@pytest.fixture
+def prot_dna_test_data():
+    """ This is the csv_data class storing the basic prot-dna test csv """
+    data = csv_data("tests/test_data/prot_dna_experiment/test.csv", ProtDnaToFloatExperiment)
+    data.data_length = 2
+    return data
+
+@pytest.fixture
+def prot_dna_test_data_with_split():
+    """ This is the csv_data class storing the basic prot-dna test csv with split """
+    data = csv_data("tests/test_data/prot_dna_experiment/test_with_split.csv", ProtDnaToFloatExperiment)
+    data.data_length = 3
+    return data
+
+@pytest.fixture
+def dna_config():
+    """ This is the config file for the dna experiment """
+    with open("tests/test_data/dna_experiment/test_config.json") as f:
+        return json.load(f)
     
-#     with open("tests/test_data/prot_dna_experiment/test_config.json") as f:
-#         configs = json.load(f)
-    
-#     return {
-#         "csv_processing": csv_processing,
-#         "configs": configs,
-#         "data_length": 2,
-#         "expected_splits": [1, 0]
-#     }
+@pytest.fixture
+def prot_dna_config():
+    """ This is the config file for the prot-dna experiment """
+    with open("tests/test_data/prot_dna_experiment/test_config.json") as f:
+        return json.load(f)
 
-# def test_dna_float_len(dna_float_setup):
-#     """Test if data is loaded with correct shape."""
-#     assert len(dna_float_setup["csv_processing"].data) == dna_float_setup["data_length"]
+# ----- run tests ----- #
 
-# def test_dna_float_add_split(dna_float_setup):
-#     """Test adding split to the data."""
-#     csv_processing = dna_float_setup["csv_processing"]
-#     csv_processing.add_split(dna_float_setup["configs"]["split"])
-    
-#     for i in range(dna_float_setup["data_length"]):
-#         assert csv_processing.data["split:split:int"][i] == dna_float_setup["expected_splits"][i]
+# TODO remove repetitive tests by using parametrize option
 
-# def test_dna_float_transform(dna_float_setup):
-#     """Test data transformation."""
-#     csv_processing = dna_float_setup["csv_processing"]
-#     csv_processing.transform(dna_float_setup["configs"]["transform"])
-    
-#     # Test transformed data
-#     expected_pets = ["cat", "dog", "cat", "dog"]
-#     expected_hola = [12.676405, 12.540016, 12.676405, 12.540016]
-#     expected_hello = ["ACTGACTGATCGATNN", "ACTGACTGATCGATNN", "NNATCGATCAGTCAGT", "NNATCGATCAGTCAGT"]
-#     expected_splits = [1, 0, 1, 0]
-    
-#     assert list(csv_processing.data["pet:meta:str"]) == expected_pets
-#     assert [round(v, 6) for v in csv_processing.data["hola:label:float"]] == expected_hola
-#     assert list(csv_processing.data["hello:input:dna"]) == expected_hello
-#     assert list(csv_processing.data["split:split:int"]) == expected_splits
+def test_len(dna_test_data, dna_test_data_long, dna_test_data_long_shuffled, prot_dna_test_data, prot_dna_test_data_with_split):
+    """ this tests the len() function properly returns the length of the csv data """
+    assert len(dna_test_data.csv_processing.data) == dna_test_data.data_length
+    assert len(dna_test_data_long.csv_processing.data) == dna_test_data_long.data_length
+    assert len(dna_test_data_long_shuffled.csv_processing.data) == dna_test_data_long_shuffled.data_length
+    assert len(prot_dna_test_data.csv_processing.data) == prot_dna_test_data.data_length
+    assert len(prot_dna_test_data_with_split.csv_processing.data) == prot_dna_test_data_with_split.data_length
 
-# # ... similar transformations for other test methods ...
+def test_add_split(dna_test_data, prot_dna_test_data, dna_config, prot_dna_config):
+    """ this tests the add_split function properly adds the split column """
+    dna_test_data.csv_processing.add_split(dna_config["split"])
+    prot_dna_test_data.csv_processing.add_split(prot_dna_config["split"])
+    print(dna_test_data.csv_processing.data)
+    assert dna_test_data.csv_processing.data["split:split:int"].to_list() == [1, 0]
+    assert prot_dna_test_data.csv_processing.data["split:split:int"].to_list() == [1, 0]
 
-# @pytest.fixture
-# def dna_loader_setup():
-#     csv_path = os.path.abspath("tests/test_data/dna_experiment/test.csv")
-#     csv_path_split = os.path.abspath("tests/test_data/dna_experiment/test_with_split.csv")
-#     experiment = DnaToFloatExperiment()
-#     csv_loader = CsvLoader(experiment, csv_path)
-    
-#     return {
-#         "csv_loader": csv_loader,
-#         "csv_path_split": csv_path_split,
-#         "experiment": experiment,
-#         "data_shape": [2, 3],
-#         "data_shape_split": [48, 4],
-#         "shape_splits": {0: 16, 1: 16, 2: 16}
-#     }
-
-# def test_loader_len(dna_loader_setup):
-#     """Test the length of the dataset."""
-#     assert len(dna_loader_setup["csv_loader"]) == dna_loader_setup["data_shape"][0]
-
-# # ... continue with other test methods ...
