33dbfc78cb383c94e241b478398687c9f2425c86
Igor Trujnara <itrujnara@wp.pl>
2024-11-08 16:22:53 +0100

    [tests] Rewrite data transformer tests to Pytest-complaint format

--- tests/test_data_transformers.py
+++ tests/test_data_transformers.py
@@ -1,9 +1,12 @@
 """Test suite for the data transformation generators."""
 
+from typing import Any
+
 import numpy as np
 import pytest
 
 from src.stimulus.data.transform.data_transformation_generators import (
+    AbstractDataTransformer,
     GaussianChunk,
     GaussianNoise,
     ReverseComplement,
@@ -11,106 +14,200 @@ from src.stimulus.data.transform.data_transformation_generators import (
 )
 
 
+class DataTransformerTest:
+    """Data for a data transformer test.
+
+    Attributes:
+        transformer: The data transformer to test.
+        params: The parameters to use for the test.
+        single_input: The single input to transform.
+        expected_single_output: The expected output for the single input.
+        multiple_inputs: The multiple inputs to transform.
+        expected_multiple_outputs: The expected outputs for the multiple inputs.
+    """
+
+    def __init__(self, # noqa: D107
+                 transformer: AbstractDataTransformer,
+                 params: dict,
+                 single_input: Any,
+                 expected_single_output: Any,
+                 multiple_inputs: Any,
+                 expected_multiple_outputs: Any):
+        self.transformer = transformer
+        self.params = params
+        self.single_input = single_input
+        self.expected_single_output = expected_single_output
+        self.multiple_inputs = multiple_inputs
+        self.expected_multiple_outputs = expected_multiple_outputs
+
+
+@pytest.fixture
+def uniform_text_masker() -> DataTransformerTest:
+    """Return a UniformTextMasker test object."""
+    transformer = UniformTextMasker(mask="N")
+    params = {"seed": 42, "probability": 0.1}
+    single_input = "ACGTACGT"
+    expected_single_output = "ACGTACNT"
+    multiple_inputs = ["ATCGATCGATCG", "ATCG"]
+    expected_multiple_outputs = ["ATCGATNGATNG", "ATCG"]
+    return DataTransformerTest(
+        transformer=transformer,
+        params=params,
+        single_input=single_input,
+        expected_single_output=expected_single_output,
+        multiple_inputs=multiple_inputs,
+        expected_multiple_outputs=expected_multiple_outputs,
+    )
+
+
+@pytest.fixture
+def gaussian_noise() -> DataTransformerTest:
+    """Return a GaussianNoise test object."""
+    transformer = GaussianNoise()
+    params = {"seed": 42, "mean": 0, "std": 1}
+    single_input = 5.0
+    expected_single_output = 5.4967141530112327
+    multiple_inputs = [1.0, 2.0, 3.0]
+    expected_multiple_outputs = [1.4967141530112327, 2.0211241446210543, 3.7835298641951802]
+    return DataTransformerTest(
+        transformer=transformer,
+        params=params,
+        single_input=single_input,
+        expected_single_output=expected_single_output,
+        multiple_inputs=multiple_inputs,
+        expected_multiple_outputs=expected_multiple_outputs,
+    )
+
+
+@pytest.fixture
+def gaussian_chunk() -> DataTransformerTest:
+    """Return a GaussianChunk test object."""
+    transformer = GaussianChunk()
+    params = {"seed": 42, "chunk_size": 10, "std": 1}
+    single_input = "AGCATGCTAGCTAGATCAAAATCGATGCATGCTAGCGGCGCGCATGCATGAGGAGACTGAC"
+    expected_single_output = "TGCATGCTAG"
+    multiple_inputs = ["AGCATGCTAGCTAGATCAAAATCGATGCATGCTAGCGGCGCGCATGCATGAGGAGACTGAC",
+                        "AGCATGCTAGCTAGATCAAAATCGATGCATGCTAGCGGCGCGCATGCATGAGGAGACTGAC"]
+    expected_multiple_outputs = ["TGCATGCTAG", "GCATGCTAGC"]
+    return DataTransformerTest(
+        transformer=transformer,
+        params=params,
+        single_input=single_input,
+        expected_single_output=expected_single_output,
+        multiple_inputs=multiple_inputs,
+        expected_multiple_outputs=expected_multiple_outputs,
+    )
+
+
+@pytest.fixture
+def reverse_complement() -> DataTransformerTest:
+    """Return a ReverseComplement test object."""
+    transformer = ReverseComplement()
+    single_input = "ACCCCTACGTNN"
+    expected_single_output = "NNACGTAGGGGT"
+    multiple_inputs = ["ACCCCTACGTNN", "ACTGA"]
+    expected_multiple_outputs = ["NNACGTAGGGGT", "TCAGT"]
+    return DataTransformerTest(
+        transformer=transformer,
+        params={},
+        single_input=single_input,
+        expected_single_output=expected_single_output,
+        multiple_inputs=multiple_inputs,
+        expected_multiple_outputs=expected_multiple_outputs,
+    )
+
+
 class TestUniformTestMasker:
     """Test suite for the UniformTextMasker class."""
-    def test_transform_single(self) -> None:
-        """Test masking a single string."""
-        transformer = UniformTextMasker(mask="N")
-        params = {"seed": 42, "probability": 0.1}
-        single_input = "ACGTACGT"
-        expected_output = "ACGTACNT"
-        transformed_data = transformer.transform(single_input, **params)
-        assert isinstance(transformed_data, str)
-        assert transformed_data == expected_output
 
-    def test_transform_multiple(self) -> None:
+    @pytest.mark.parametrize("test_data_name", ["uniform_text_masker"])
+    def test_transform_single(self, request: Any, test_data_name: str) -> None:
+        """Test masking a single string.
+
+        Args:
+            test_data: The test data to use.
+        """
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform(test_data.single_input, **test_data.params)
+        assert isinstance(transformed_data, str)
+        assert transformed_data == test_data.expected_single_output
+
+    @pytest.mark.parametrize("test_data_name", ["uniform_text_masker"])
+    def test_transform_multiple(self, request: Any, test_data_name: str) -> None:
         """Test masking multiple strings."""
-        transformer = UniformTextMasker(mask="N")
-        params = {"seed": 42, "probability": 0.1}
-        multiple_inputs = ["ATCGATCGATCG", "ATCG"]
-        expected_outputs = ["ATCGATNGATNG", "ATCG"]
-        transformed_data = transformer.transform_all(multiple_inputs, **params)
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform_all(test_data.multiple_inputs, **test_data.params)
         assert isinstance(transformed_data, list)
         for item in transformed_data:
             assert isinstance(item, str)
-        assert transformed_data == expected_outputs
+        assert transformed_data == test_data.expected_multiple_outputs
 
 
 class TestGaussianNoise:
     """Test suite for the GaussianNoise class."""
-    def test_transform_single(self) -> None:
-        """Test transforming a single float."""
-        transformer = GaussianNoise()
-        params = {"seed": 42, "mean": 0, "std": 1}
-        single_input = 5.0
-        expected_output = 5.4967141530112327
-        transformed_data = transformer.transform(single_input, **params)
-        assert isinstance(transformed_data, float)
-        assert round(transformed_data, 7) == round(expected_output, 7)
 
-    def test_transform_multiple(self) -> None:
+    @pytest.mark.parametrize("test_data_name", ["gaussian_noise"])
+    def test_transform_single(self, request: Any, test_data_name: str) -> None:
+        """Test transforming a single float."""
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform(test_data.single_input, **test_data.params)
+        assert isinstance(transformed_data, float)
+        assert round(transformed_data, 7) == round(test_data.expected_single_output, 7)
+
+    @pytest.mark.parametrize("test_data_name", ["gaussian_noise"])
+    def test_transform_multiple(self, request: Any, test_data_name: DataTransformerTest) -> None:
         """Test transforming multiple floats."""
-        transformer = GaussianNoise()
-        params = {"seed": 42, "mean": 0, "std": 1}
-        multiple_inputs = [1.0, 2.0, 3.0]
-        expected_outputs = [1.764052345967664, 0.4001572083672233, 0.9787379841057392]
-        transformed_data = transformer.transform_all(multiple_inputs, **params)
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform_all(test_data.multiple_inputs, **test_data.params)
         assert isinstance(transformed_data, np.ndarray)
         for item in transformed_data:
             assert isinstance(item, float)
-        assert len(transformed_data) == len(expected_outputs)
-        for item, expected in zip(transformed_data, expected_outputs):
+        assert len(transformed_data) == len(test_data.expected_outputs)
+        for item, expected in zip(transformed_data, test_data.expected_multiple_outputs):
             assert round(item, 7) == round(expected, 7)
 
 
 class TestGaussianChunk:
     """Test suite for the GaussianChunk class."""
-    def test_transform_single(self) -> None:
+
+    @pytest.mark.parametrize("test_data_name", ["gaussian_chunk"])
+    def test_transform_single(self, request: Any, test_data_name: str) -> None:
         """Test transforming a single string."""
-        transformer = GaussianChunk()
-        params = {"seed": 42, "chunk_size": 10, "std": 1}
-        single_input = "AGCATGCTAGCTAGATCAAAATCGATGCATGCTAGCGGCGCGCATGCATGAGGAGACTGAC"
-        expected_output = "TGCATGCTAG"
-        transformed_data = transformer.transform(single_input, **params)
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform(test_data.single_input, **test_data.params)
         assert isinstance(transformed_data, str)
         assert len(transformed_data) == 10
-        assert transformed_data == expected_output
+        assert transformed_data == test_data.expected_single_output
 
-    def test_transform_multiple(self) -> None:
+    @pytest.mark.parametrize("test_data_name", ["gaussian_chunk"])
+    def test_transform_multiple(self, request: Any, test_data_name: str) -> None:
         """Test transforming multiple strings."""
-        transformer = GaussianChunk()
-        params = {"seed": 42, "chunk_size": 10, "std": 1}
-        multiple_inputs = [
-            "AGCATGCTAGCTAGATCAAAATCGATGCATGCTAGCGGCGCGCATGCATGAGGAGACTGAC",
-            "AGCATGCTAGCTAGATCAAAATCGATGCATGCTAGCGGCGCGCATGCATGAGGAGACTGAC",
-        ]
-        expected_outputs = ["TGCATGCTAG", "GCATGCTAGC"]
-        transformed_data = transformer.transform_all(multiple_inputs, **params)
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform_all(test_data.multiple_inputs, **test_data.params)
         assert isinstance(transformed_data, list)
         for item in transformed_data:
             assert isinstance(item, str)
             assert len(item) == 10
-        assert transformed_data == expected_outputs
+        assert transformed_data == test_data.expected_multiple_outputs
 
 
 class TestReverseComplement:
     """Test suite for the ReverseComplement class."""
-    def test_transform_single(self) -> None:
-        """Test transforming a single string."""
-        transformer = ReverseComplement()
-        single_input = "ACCCCTACGTNN"
-        expected_output = "NNACGTAGGGGT"
-        transformed_data = transformer.transform(single_input)
-        assert isinstance(transformed_data, str)
-        assert transformed_data == expected_output
 
-    def test_transform_multiple(self) -> None:
+    @pytest.mark.parametrize("test_data_name", ["reverse_complement"])
+    def test_transform_single(self, request: Any, test_data_name: str) -> None:
+        """Test transforming a single string."""
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform(test_data.single_input, **test_data.params)
+        assert isinstance(transformed_data, str)
+        assert transformed_data == test_data.expected_single_output
+
+    @pytest.mark.parametrize("test_data_name", ["reverse_complement"])
+    def test_transform_multiple(self, request: Any, test_data_name: str) -> None:
         """Test transforming multiple strings."""
-        transformer = ReverseComplement()
-        multiple_inputs = ["ACCCCTACGTNN", "ACTGA"]
-        expected_outputs = ["NNACGTAGGGGT", "TCAGT"]
-        transformed_data = transformer.transform_all(multiple_inputs)
+        test_data = request.getfixturevalue(test_data_name)
+        transformed_data = test_data.transformer.transform_all(test_data.multiple_inputs, **test_data.params)
         assert isinstance(transformed_data, list)
         for item in transformed_data:
             assert isinstance(item, str)
-        assert transformed_data == expected_outputs
+        assert transformed_data == test_data.expected_multiple_outputs
