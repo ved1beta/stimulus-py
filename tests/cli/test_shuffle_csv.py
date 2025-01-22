e23f3ed3230c80da8e7c29cbe618d776e0efac88
Igor Trujnara <itrujnara@wp.pl>
2025-01-22 15:56:19 +0100

    Add test for shuffle_csv

---
+++ tests/cli/test_shuffle_csv.py
@@ -0,0 +1,47 @@
+"""Tests for the shuffle_csv CLI command."""
+
+import hashlib
+import pathlib
+import tempfile
+
+import pytest
+
+from src.stimulus.cli.shuffle_csv import main
+
+
+# Fixtures
+@pytest.fixture
+def correct_yaml_path() -> str:
+    """Fixture that returns the path to a correct YAML file."""
+    return "tests/test_data/titanic/titanic_sub_config.yaml"
+
+
+@pytest.fixture
+def correct_csv_path() -> str:
+    """Fixture that returns the path to a correct CSV file."""
+    return "tests/test_data/titanic/titanic_stimulus.csv"
+
+
+# Test cases
+test_cases = [
+    ("correct_csv_path", "correct_yaml_path", None),
+]
+
+
+# Tests
+@pytest.mark.parametrize(("csv_type", "yaml_type", "error"), test_cases)
+def test_shuffle_csv(
+    request: pytest.FixtureRequest, snapshot: pytest.fixture, csv_type: str, yaml_type: str, error: Exception | None,
+) -> None:
+    """Tests the CLI command with correct and wrong YAML files."""
+    csv_path = request.getfixturevalue(csv_type)
+    yaml_path = request.getfixturevalue(yaml_type)
+    tmpdir = pathlib.Path(tempfile.gettempdir())
+    if error:
+        with pytest.raises(error):
+            main(csv_path, yaml_path, tmpdir / "test.csv")
+    else:
+        assert main(csv_path, yaml_path, tmpdir / "test.csv") is None
+        with open(tmpdir / "test.csv") as file:
+            hash = hashlib.md5(file.read().encode()).hexdigest()  # noqa: S324
+        assert hash == snapshot
