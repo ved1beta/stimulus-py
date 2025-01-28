b63fd90da7fd08890d7ced687c995d602d23d097
mgrapotte <mathysgrapotte@gmail.com>
2025-01-28 14:53:49 +0100

    test(model_schema): added a test for yaml_config validation

--- tests/utils/test_model_yaml.py
+++ tests/utils/test_model_yaml.py
@@ -1,8 +1,7 @@
-"Tests for YAML model handling functionalities"
+"""Tests for YAML model handling functionalities"""
 
 import pytest
 import yaml
-from ray import tune
 
 from src.stimulus.utils import yaml_model_schema
 
@@ -12,9 +11,10 @@ def titanic_model_yaml_path() -> str:
     """Get path to Titanic model YAML file."""
     return "tests/test_model/titanic_model_cpu.yaml"
 
+
 def test_model_yaml(titanic_model_yaml_path: str) -> None:
     """Test model YAML file."""
-    with open(titanic_model_yaml_path, "r") as file:
+    with open(titanic_model_yaml_path) as file:
         model_config = yaml.safe_load(file)
     model = yaml_model_schema.Model.model_validate(model_config)
     assert model is not None
